54ef1ef9c378fd48275f2638249a5e81273b57d2
Sam Wong <sam.wong.uk@gmail.com>
2015-01-12 20:45:48 +0000

    init commit

---
+++ SwiftCopters/GameOverScene.swift
@@ -0,0 +1,36 @@
+//
+//  GameOverScene.swift
+//  SwiftCopters
+//
+
+import SpriteKit
+
+class GameOverScene: SKScene {
+    
+    var viewController:GameViewController?
+    
+    //Init
+    override func didMoveToView(view: SKView) {
+        
+        let nodeLabel = SKLabelNode(fontNamed: "Lobster 1.4")
+        nodeLabel.text = "game over"
+        nodeLabel.fontSize = 50
+        nodeLabel.fontColor = SKColor.whiteColor()
+        nodeLabel.position = CGPointMake(self.frame.width/2, self.frame.height/3*2)
+        nodeLabel.alpha = 0.0
+        nodeLabel.runAction(SKAction.fadeAlphaTo(1.0, duration: 1.0))
+        self.addChild(nodeLabel)
+        
+        let spriteFace = SKSpriteNode(imageNamed: "booGameOver")
+        spriteFace.size = CGSizeMake(spriteFace.size.width/2, spriteFace.size.height/2)
+        spriteFace.position = CGPointMake(self.frame.width/2, -spriteFace.size.height/2)
+        spriteFace.runAction(SKAction.moveToY(spriteFace.size.height/2, duration: 0.7))
+        self.addChild(spriteFace)
+        
+        self.backgroundColor = SKColor(red: 20/255, green: 129/255, blue: 197/255, alpha: 1.0)
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        viewController?.presentGameScene()
+    }
+}
